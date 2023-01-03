ce9eb4826e158cc0ace91400e14c963f20f0b68a
Goun Lee <goun.lee@lge.com>
2023-01-03 15:22:25 +0900

    Update copyright year

--- QMLFileType.js
+++ QMLFileType.js
@@ -1,7 +1,7 @@
 /*
  * QMLFileType.js - Represents a collection of QML files
  *
- * Copyright (c) 2020-2022, JEDLSoft
+ * Copyright (c) 2020-2023, JEDLSoft
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
